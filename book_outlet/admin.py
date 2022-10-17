769335677958ff5622eae445c2f92c648a182d35
Toungchai Sriangthong <114647008+Kingleopard168@users.noreply.github.com>
2022-10-17 15:32:02 +0700

    How to use Admin Model
    How to use Admin Model

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,3 +1,13 @@
+from http.cookies import BaseCookie
+import imp
 from django.contrib import admin
 
+from.models import Book
+
 # Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    #readonly_fields = ("slug",)
+    prepopulated_fields = {"slug": ("title",)}
+
+admin.site.register(Book, BookAdmin)
