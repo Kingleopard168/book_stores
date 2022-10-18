d02620238a83956fd3ac9c10a3a1a7baf28f0150
Toungchai Sriangthong <114647008+Kingleopard168@users.noreply.github.com>
2022-10-18 16:57:53 +0700

    More function admin django
    More function admin django

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -9,5 +9,6 @@ from.models import Book
 class BookAdmin(admin.ModelAdmin):
     #readonly_fields = ("slug",)
     prepopulated_fields = {"slug": ("title",)}
-
+    list_filter = ("author","rating",)
+    list_display = ("title", "author",)
 admin.site.register(Book, BookAdmin)
